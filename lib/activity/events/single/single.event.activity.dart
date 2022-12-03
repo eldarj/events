ad79fd48666fbd94baa51636f417bb2358d7f544
Eldar Jahijagic <ejahijagic@unifonic.com>
2022-12-03 15:15:43 +0400

    Finish single event page

--- lib/activity/events/single/single.event.activity.dart
+++ lib/activity/events/single/single.event.activity.dart
@@ -6,8 +6,10 @@ import 'package:dubai_events/main.dart';
 import 'package:dubai_events/service/data/events.model.dart';
 import 'package:dubai_events/shared/base/base.state.dart';
 import 'package:dubai_events/shared/maps/map.component.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:maps_launcher/maps_launcher.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SingleEventActivity extends StatefulWidget {
   final EventModel event;
@@ -120,43 +122,133 @@ class SingleEventActivityState extends BaseState<SingleEventActivity> {
           child: EventDetailsPartial(event: widget.event)),
       Container(height: 5, color: Colors.grey.shade100),
       Container(
+          color: Colors.white,
+          padding:
+          const EdgeInsets.only(top: 15, bottom: 10, left: 10, right: 10),
+        child: Text("Description", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey.shade500))
+      ),
+      Container(
         color: Colors.white,
         padding:
-            const EdgeInsets.only(top: 15, bottom: 15, left: 10, right: 10),
+            const EdgeInsets.only(left: 10, right: 10),
         child: Text(widget.event.shortDescription, style: TextStyle(color: Colors.grey.shade500)),
       ),
-      Container(height: 5, color: Colors.grey.shade100),
       Container(
         color: Colors.white,
         padding:
         const EdgeInsets.only(top: 15, bottom: 15, left: 10, right: 10),
         child: Text(widget.event.description, style: TextStyle(color: Colors.grey.shade500)),
-      ),
+      )
+      ,
       Container(height: 5, color: Colors.grey.shade100),
-
-      widget.event.eventLocation?.mapImageUrl != null ? GestureDetector(
-        onTap: () {
-          MapsLauncher.launchCoordinates(widget.event.eventLocation?.latitude ?? 0,
-              widget.event.eventLocation?.longitude ?? 0, widget.event.eventLocation?.name);
-        },
-        child: Container(
-          padding: const EdgeInsets.all(15),
-          color: Colors.white,
-          child: CachedNetworkImage(
-            fit: BoxFit.cover,
-            alignment: Alignment.center,
-            imageUrl: widget.event.eventLocation?.mapImageUrl ?? "",
-            placeholder: (context, url) => Container(
-                alignment: Alignment.center,
-                height: 25,
-                width: 25,
-                child: const CircularProgressIndicator(
-                    color: Colors.redAccent)),
-            errorWidget: (context, url, error) =>
-            const Icon(Icons.broken_image_outlined, color: Colors.grey),
+      Material(
+        color: Colors.white,
+        child: InkWell(
+          onTap: () {
+            launchUrl(Uri.parse(widget.event.reservationUrl));
+          },
+          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Container(
+                padding: const EdgeInsets.only(top: 15, left: 10, right: 10),
+                child: Text("Reservations", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey.shade500))
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 5, bottom: 15, left: 10, right: 10),
+              child: Text(widget.event.reservationUrl, style: TextStyle(color: Colors.grey.shade500)),
+            ),
+          ]),
+        )
+      )
+      ,
+      Container(height: 5, color: Colors.grey.shade100),
+      Container(
+        color: Colors.white,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Container(
+              padding: const EdgeInsets.only(top: 15, left: 10, right: 10),
+              child: Text("Contact", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey.shade500))
+          ),
+          Container(
+            padding: const EdgeInsets.only(top: 5, bottom: 15),
+            child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              contactTile(Icons.phone, widget.event.eventContact?.phoneNumber, () {
+                launchUrl(Uri(scheme: "tel", path: widget.event.eventContact?.phoneNumber ?? ""));
+              }),
+              contactTile(Icons.whatsapp, widget.event.eventContact?.whatsappNumber, () {
+                launchUrl(Uri.parse("whatsapp://send?phone=${widget.event.eventContact?.whatsappNumber}" ?? ""));
+              }),
+              contactTile(Icons.phone, widget.event.eventContact?.email, () {
+                launchUrl(Uri.parse("mailto:${widget.event.eventContact?.whatsappNumber}" ?? ""));
+              }),
+              contactTile(Icons.facebook_rounded, widget.event.eventContact?.facebookUrl, () {
+                launchUrl(Uri.parse(widget.event.eventContact?.facebookUrl ?? ""));
+              }),
+              contactTile(Icons.info_outline, widget.event.eventContact?.twitterUrl, () {
+                launchUrl(Uri.parse(widget.event.eventContact?.twitterUrl ?? ""));
+              }),
+              contactTile(Icons.info_outline, widget.event.eventContact?.instagramUrl, () {
+                launchUrl(Uri.parse(widget.event.eventContact?.instagramUrl ?? ""));
+              }),
+            ]),
+          ),
+        ]),
+      )
+      ,
+      Container(height: 5, color: Colors.grey.shade100),
+      widget.event.eventLocation?.mapImageUrl != null ? Material(
+        color: Colors.white,
+        child: InkWell(
+          onTap: () {
+            MapsLauncher.launchCoordinates(widget.event.eventLocation?.latitude ?? 0,
+                widget.event.eventLocation?.longitude ?? 0, widget.event.eventLocation?.name);
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                  padding: const EdgeInsets.only(top: 15, bottom: 0, left: 10, right: 10),
+                  child: Text("Location", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey.shade500))
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                width: deviceMediaSize.width - 5,
+                child: CachedNetworkImage(
+                  fit: BoxFit.cover,
+                  alignment: Alignment.center,
+                  imageUrl: widget.event.eventLocation?.mapImageUrl ?? "",
+                  placeholder: (context, url) => Container(
+                      alignment: Alignment.center,
+                      height: 25,
+                      width: 25,
+                      child: const CircularProgressIndicator(
+                          color: Colors.redAccent)),
+                  errorWidget: (context, url, error) =>
+                  const Icon(Icons.broken_image_outlined, color: Colors.grey),
+                ),
+              ),
+            ],
           ),
         ),
       ) : Container(),
     ]);
+  }
+
+
+  Widget contactTile(icon, text, onTap) {
+    return text != null && text.length > 0 ? Material(
+      color: Colors.white,
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.only(top: 10, bottom: 10, right: 10, left: 10),
+          child: Row(children: [
+            Container(
+                margin: const EdgeInsets.only(right: 10),
+                child: Icon(icon, color: Colors.grey, size: 20)),
+            Text(text ?? "", style: const TextStyle(color: Colors.grey, fontSize: 16)),
+          ]),
+        ),
+      ),
+    ) : Container();
   }
 }
